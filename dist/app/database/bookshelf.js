caa888fa9874b8ad2dc554f7a5c9bd13e78ba500
alexj01 <janus.alex@gmail.com>
2017-06-22 23:03:50 -0400

    dist to git idnore

--- dist/app/database/bookshelf.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const knex = require('knex')(require('./knexfile')[process.env.ENV || 'development']);
-const bookshelf = require('bookshelf')(knex);
-bookshelf.plugin('registry');
-module.exports = bookshelf;
